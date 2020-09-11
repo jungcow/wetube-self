12a8d5bd30b536120cf33e0bc9a23275d4f35591
jungcow <60311340+jungcow@users.noreply.github.com>
2020-09-11 10:09:45 +0900

    recommit for upload

--- router/userRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { editProfile, changePassword, userDetail } from '../controllers/userController';
-import routes from '../routes';
-
-export const userRouter = express.Router();
-
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail(), userDetail);
